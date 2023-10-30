5dbd31458fc4362a313ba9b683f513c6f1905550
RozverA <maximlut1@vk.com>
2023-10-30 15:52:20 +0800

    fix error len in funx

--- rz_lib.c
+++ rz_lib.c
@@ -7,8 +7,9 @@ BYTE num_to_byte(DWORD num, BYTE len, BYTE* dst, BYTE side) //
 
     for (BYTE i = 0; i < len;i++)
     {
-        if(side)    {*dst = num >> (8 * ( len ) - ( i * 8 ));}
+        if(side)    {*dst = num >> (( 8 * (len - 1 ) ) - ( i * 8 ));}
         else        {*dst = num >> ( i * 8 );}
+        dst++;
     }
     return SUCCES;
 }
